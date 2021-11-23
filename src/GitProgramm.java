3f2e371c1bddd5f0829b60c3cabfab8df115f9b0
Aniket-4545 <chuadhariam007>
2021-11-23 23:04:07 +0530

    Do modification in this program

---
+++ src/GitProgramm.java
@@ -0,0 +1,11 @@
+
+public class GitProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" my first git project");
+System.out.println(" hello Everyone");
+System.out.println(" staging area");
+	}
+
+}
